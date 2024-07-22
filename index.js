7f146ff2d94387d5ee5498927a36019b08088606
Ryan <ryanshaunsmith@gmail.com>
2024-07-22 17:33:45 +0100

    removed all code barr comments and rewrote it

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import express from "express"; // import the express modules using ES Module syntax
-const app = express() // set a variable to store the express function to make accessing it later cleaner
-const port = 3000 // set the port for the server to be accessed from 
+const app = express(); // set a variable to store the express function to make accessing it later cleaner
+const port = 3000; // set the port for the server to be accessed from 
 
 // creating a GET request handler when the root/default file is accessed
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     // respond with a message
-    res.send('Hello World of Wonder!')
+    res.send("Did I manage to remember this all??????");
 })
+
 // creating a handler to open the port for access
 app.listen(port, () => {
     // log a message to confirm the server has started
-    console.log(`Example app listening on port ${port}`)
+    console.log(`This app is now listening on ${port}...hopefully`);
 })
