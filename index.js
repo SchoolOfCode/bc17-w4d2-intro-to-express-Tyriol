d18202dc7feb9ec23a1e996acdf7123992f4d775
Ryan <ryanshaunsmith@gmail.com>
2024-07-22 17:22:37 +0100

    added comment to explain what the file is doing...I think

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello You!");
+import express from "express"; // import the express modules using ES Module syntax
+const app = express() // set a variable to store the express function to make accessing it later cleaner
+const port = 3000 // set the port for the server to be accessed from 
+
+// creating a GET request handler when the root/default file is accessed
+app.get('/', (req, res) => {
+    // respond with a message
+    res.send('Hello World of Wonder!')
+})
+// creating a handler to open the port for access
+app.listen(port, () => {
+    // log a message to confirm the server has started
+    console.log(`Example app listening on port ${port}`)
+})
